936d054539531d3571a16bc713778bb01bcee9cc
shinghao <sohshinghao@gmail.com>
2023-11-23 01:29:57 +0800

    Update 70-climbing-stairs.cpp

--- cpp/70-climbing-stairs.cpp
+++ cpp/70-climbing-stairs.cpp
@@ -1,3 +1,12 @@
+/*
+    Time -> O(n), Space -> O(1)
+    
+    dp[n] = dp[n - 1] + dp[n - 2]
+    Reach n step in 2 ways: 
+        1) 1 step from n-1, 
+        2) 2 steps from n-2
+*/
+
 class Solution {
 public:
     int climbStairs(int n) {
