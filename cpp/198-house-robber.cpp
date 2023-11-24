14bc1af01e1e92c10f8dc09290b8dfa6c25206b3
shinghao <sohshinghao@gmail.com>
2023-11-25 01:52:28 +0800

    Create 198-house-robber.cpp

---
+++ cpp/198-house-robber.cpp
@@ -0,0 +1,21 @@
+// Time -> O(n) , Space -> O(1)
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        nums[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            nums[i] = max(nums[i - 1], nums[i] + nums[i - 2]);
+        } 
+
+        return nums.back();
+    }
+};
