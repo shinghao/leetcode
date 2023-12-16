6fc81241ccbbbc97c30a0cbdfeed90bd767f857e
shinghao <sohshinghao@gmail.com>
2023-12-16 11:54:16 +0800

    Create 242-valid-anagram.cpp

---
+++ cpp/242-valid-anagram.cpp
@@ -0,0 +1,28 @@
+// Time - O(n), Space - O(26)
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {};
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }    
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
