8f2f0d5c7128f315cf31848e5a393a0ff012f6cb
shinghao <sohshinghao@gmail.com>
2024-01-05 17:33:40 +0800

    Create 167-two-sum-ii---input-array-is-sorted.cpp

---
+++ cpp/167-two-sum-ii---input-array-is-sorted.cpp
@@ -0,0 +1,24 @@
+// Time -> O(n), Space -> O(1)
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+    int low = 0, high = numbers.size() - 1;
+
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return {low + 1, high + 1};
+            } else if (sum < target) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+
+        return {-1, -1};
+    }
+};
